13d8544f6cb6bdf0056dd7d86115837cc99a98cc
amir-fatyhow <100147741+amir-fatyhow@users.noreply.github.com>
2022-08-22 14:42:56 +0300

    Update DirectorDbStorage.java

--- src/main/java/ru/yandex/practicum/filmorate/repository/DirectorDbStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/repository/DirectorDbStorage.java
@@ -74,11 +74,6 @@ public class DirectorDbStorage implements DirectorStorage {
         String sql = "SELECT * FROM DIRECTORS WHERE ID IN " +
                 "(SELECT DIRECTOR_ID FROM FILMS_DIRECTORS WHERE FILM_ID = ?)";
         return jdbcTemplate.query(sql, new BeanPropertyRowMapper<>(Director.class), id);
-
-    public List<Director> getDirectorsByFilmId(long id) throws FilmNotFound {
-        String sql = "SELECT * FROM DIRECTORS WHERE ID IN " +
-                "(SELECT DIRECTOR_ID FROM FILMS_DIRECTORS WHERE FILM_ID = ?)";
-        return jdbcTemplate.query(sql, new DirectorRowMapper(), id);
     }
 
 }
